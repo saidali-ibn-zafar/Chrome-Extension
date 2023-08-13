d9f294e043d7571711ead5b140df2f64646fc7c9
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2023-08-12 21:47:25 -1200

    gotMessage function added

--- chromeEx-programming-with-text/02-background-script/content.js
+++ chromeEx-programming-with-text/02-background-script/content.js
@@ -1,1 +1,13 @@
 console.log("Chrome Extension is ready to go!");
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse) {
+  console.log(message.txt);
+  if (message.txt === "hello") {
+    let paragraphs = document.getElementsByTagName("p");
+    for (el of paragraphs) {
+      el.style["background-color"] = "#FF00FF";
+    }
+  }
+}
